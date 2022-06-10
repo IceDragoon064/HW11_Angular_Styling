4a9d8f7dab94c840ad07b30f6ac92bc708f77160
Leon Lu <ltlu1993@gmail.com>
2022-06-09 21:28:55 -0400

    Exercise 1 with screenshot

--- myapp/src/app/app.component.ts
+++ myapp/src/app/app.component.ts
@@ -1,23 +1,35 @@
+import { getCurrencySymbol } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: `<label [class.danger]="foodWarning">Has food allergy.</label>
+  template: `
+  <label [class.danger]="foodWarning">Has food allergy.</label>
   <input type="checkbox" [(ngModel)]="foodWarning" />
+  <br>
+  <label [class.greenDanger]="greenWarning">Turn this green.</label>
+  <input type="checkbox" [(ngModel)]="greenWarning"/>
   `,
 
   styles: [`
     .danger {
     color:orange;
     font-weight:bold;
+    }
+
+    .greenDanger{
+      color:green;
+      font-weight:bold;
     }`
   ]
 })
 
 export class AppComponent {
   foodWarning:Boolean
+  greenWarning:Boolean
   constructor() {
     // Set default value to false.
     this.foodWarning = false;
+    this.greenWarning = false;
   }
 }
