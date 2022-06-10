b6fd8673fef383cb663f314895b62b0dacb4f195
Leon Lu <ltlu1993@gmail.com>
2022-06-09 21:16:49 -0400

    Example 2

--- myapp/src/app/app.component.ts
+++ myapp/src/app/app.component.ts
@@ -2,9 +2,22 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `<label [class.danger]="foodWarning">Has food allergy.</label>
+  <input type="checkbox" [(ngModel)]="foodWarning" />
+  `,
+
+  styles: [`
+    .danger {
+    color:orange;
+    font-weight:bold;
+    }`
+  ]
 })
+
 export class AppComponent {
-  title = 'myapp';
+  foodWarning:Boolean
+  constructor() {
+    // Set default value to false.
+    this.foodWarning = false;
+  }
 }
