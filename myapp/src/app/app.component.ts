1e4f448c09d8def2d576de19a8e16c4671e655a2
Leon Lu <ltlu1993@gmail.com>
2022-06-09 21:49:26 -0400

    Exercise 3
    Line 7: changed ngIf condition from || to &&

--- myapp/src/app/app.component.ts
+++ myapp/src/app/app.component.ts
@@ -1,34 +1,31 @@
 import { Component } from '@angular/core';
-
 @Component({
-  selector: 'app-root',
-  template: `
-  <input type="checkbox" [(ngModel)]="doesNotExercise"
-  (change)="adjustCSS()"/>Does not exercise.
-  <input type="checkbox" [(ngModel)]="over55" (change)="adjustCSS()"/>
-  Is over 55.
-  <div *ngIf="doesNotExercise || over55" [ngClass]="myClasses" >
-  Is at risk of heart disease.</div>
-  `,
-  styles: [`
-    .warning {
+selector: 'app-root',
+template: `
+  <input type="checkbox" [(ngModel)]="doesNotExercise" (change)="adjustCSS()"/>Does not exercise.
+  <input type="checkbox" [(ngModel)]="over55" (change)="adjustCSS()"/> Is over 55.
+  <div *ngIf="doesNotExercise && over55" [ngClass]="myClasses"> Is at risk of heart disease.</div>
+`,
+styles: [`
+  .warning {
     font-weight:bold;
   }
-
   .danger {
     color:red;
   }`
-  ]
+]
 })
 
-  export class AppComponent {
-    title = 'app';
-    doesNotExercise = false;
-    over55 = false;
-    myClasses = {
-      warning: false,
-      danger: false
-    }
-    adjustCSS() {
-    }
+export class AppComponent {
+  title = 'app';
+  doesNotExercise = false;
+  over55 = false;
+  myClasses = {
+   warning: true,
+   danger: true
+  }
+
+  adjustCSS() {
+
+  }
 }
